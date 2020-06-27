dffc10f9d4274a618c10cdfcd539b45ec1f68c6e
Rubrex <rubrex96@hotmail.com>
2020-06-27 18:18:18 +0600

    Updates

---
+++ precache-manifest.c09e7a3d8174157ab926e1f9c92cdf0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c03d48d87068397fa3d9213368c795",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dba7e7a69c9d552bde03",
+    "url": "/portfolio/static/css/main.f09a1aa6.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba7e7a69c9d552bde03",
+    "url": "/portfolio/static/js/main.67a36613.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b78b5fc825fee85cbaecbbb7a8c4f59e",
+    "url": "/portfolio/static/media/bg.b78b5fc8.jpg"
+  },
+  {
+    "revision": "b53e01d443da98555aadd4b80e229615",
+    "url": "/portfolio/static/media/logo.b53e01d4.svg"
+  }
+]);
